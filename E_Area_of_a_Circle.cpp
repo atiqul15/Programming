ed4c15cd5fa9275b2efb9600e237bd5745539a89
Sayem <atiqulislamsayem@gmail.com>
2024-10-26 03:11:06 +0600

    sifat did

--- E_Area_of_a_Circle.cpp
+++ E_Area_of_a_Circle.cpp
@@ -1,6 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main (){
-
-}  
-
+#define PI 3.141592653
+int main(){
+    double i;
+    cin >> i;
+   // scanf("%lf", &i);
+   // double a=PI*i*i;
+cout << fixed << setprecision(9) << PI * i *
+    //printf("%.9lf",PI*i*i);  
+}
